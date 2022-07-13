a8700db0ddfc56ab3628d2c25875725c4f6afd9d
Olumide Adenaike <49565225+olustixs01@users.noreply.github.com>
2022-07-13 10:57:20 +0100

    Create LifeRemainderCalculator.js

---
+++ LifeRemainderCalculator.js
@@ -0,0 +1,11 @@
+function lifeInWeeks(age) {
+
+   var remainingYears=(90-age);
+   var daysLeft=(remainingYears*365);
+   var weeksLeft=(remainingYears*52);
+   var monthsLeft=(remainingYears*12);
+   
+   console.log ("You have " + daysLeft + " days, " + weeksLeft + " weeks, and "  + monthsLeft + " months left. ");
+  return 
+} 
+ lifeInWeeks(28
