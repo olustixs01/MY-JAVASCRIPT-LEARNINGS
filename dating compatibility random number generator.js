cf25fd3a06513f8f741305e74847eba461abaf3f
Olumide Adenaike <49565225+olustixs01@users.noreply.github.com>
2022-07-13 10:55:43 +0100

    Create dating compatibility random number generator.js

---
+++ dating compatibility random number generator.js
@@ -0,0 +1,6 @@
+var firstName = prompt("what is your name");
+var secondName = prompt("what is your crush's name");
+var n = Math.random();
+n = n*100;
+n = Math.floor(n)+ 1;
+console.log("Congratulations " + firstName + " and " + secondName + " are "+ n + "% compatible")
